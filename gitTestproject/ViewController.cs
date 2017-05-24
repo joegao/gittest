a6867fc232a9b1d9c8a78d5233a835a16eba5437
Joe Gao <joe.gao@cma.ca>
2017-05-24 19:18:55 -0400

    the change made

--- gitTestproject/ViewController.cs
+++ gitTestproject/ViewController.cs
@@ -13,6 +13,7 @@ namespace gitTestproject
         public override void ViewDidLoad()
         {
             //the change in the login branch 2
+	    //the change made in the slave branch
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
         }
