fb33c1b5b9d886bfb0b9e1f3faded6dfc69b3455
Joe Gao <joe.gao@cma.ca>
2017-05-24 17:36:35 -0400

    text change

--- gitTestproject/ViewController.cs
+++ gitTestproject/ViewController.cs
@@ -12,6 +12,7 @@ namespace gitTestproject
 
         public override void ViewDidLoad()
         {
+
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
         }
